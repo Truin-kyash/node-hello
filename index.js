0336dbf9df4d5fd783e871d63a2e9ef16c207c9f
Truin-kyash <suyash.keshari@truinc.com>
2025-02-27 09:46:21 +0530

    Update index.js for Interlink

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  res.setHeader('Content-Type', 'text/html'); // Set the content type to HTML
+  const msg = `
+    <html>
+      <head>
+        <title>Hello Node</title>
+      </head>
+      <body>
+        <h1>Hello Node!</h1>
+        <p>Click the link to visit Google: <a href="https://www.google.co.in/" target="_blank">Go to Google</a></p>
+      </body>
+    </html>
+  `;
   res.end(msg);
 });
 
